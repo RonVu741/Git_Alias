b39735d9f5e544b7e811248e87aad8af025eebe0
sinhvien <sinhvien>
2021-04-10 09:08:56 +0700

    push project NTP_Git

---
+++ NTP_Git/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NTP_Git.MvcApplication" Language="C#" %>
---
+++ NTP_Git/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NTP_Git.Startup))]
+namespace NTP_Git
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
